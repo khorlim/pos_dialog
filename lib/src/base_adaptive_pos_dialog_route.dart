140df5bee40969b6421d9579a4052f733db04427
khorlim <khorlim2001@gmail.com>
2025-04-04 11:07:30 +0800

    up

--- lib/src/base_adaptive_pos_dialog_route.dart
+++ lib/src/base_adaptive_pos_dialog_route.dart
@@ -3,46 +3,17 @@ import 'package:flutter/widgets.dart';
 
 abstract class BaseAdaptivePosDialogRoute<T> extends PageRoute<T> {
   BaseAdaptivePosDialogRoute({
-    this.closeProgressThreshold,
-    required this.followerAnchor,
-    required this.builder,
-    required this.position,
-    this.useSafeArea = false,
-    this.offset = Offset.zero,
-    this.avoidOverflow = false,
-    this.scrollController,
     this.barrierLabel,
-    this.secondAnimationController,
     this.modalBarrierColor,
     this.isDismissible = true,
-    this.enableDrag = true,
-    required this.dialogSize,
-    required this.expanded,
-    this.bounce = false,
     this.animationCurve,
     this.duration = const Duration(milliseconds: 400),
     super.settings,
   });
 
-  final double? closeProgressThreshold;
-  final WidgetBuilder builder;
-  final bool expanded;
-  final bool bounce;
   final Color? modalBarrierColor;
   final bool isDismissible;
-  final bool enableDrag;
-  final ScrollController? scrollController;
-  final Size dialogSize;
-
-  final Alignment followerAnchor;
-  final Offset position;
-  final bool useSafeArea;
-  final Offset offset;
-  final bool avoidOverflow;
-
   final Duration duration;
-
-  final AnimationController? secondAnimationController;
   final Curve? animationCurve;
 
   @override
